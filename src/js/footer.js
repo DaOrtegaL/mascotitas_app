1acfdf1f6fb83d32c63ad29b6e7e2c1dd2814c2f
DaOrtegaL <daortegal97@outlook.com>
2024-10-18 13:57:35 -0500

    Se crea vista productos, se agregan las cards dinamicas y la paginación

--- src/js/footer.js
+++ src/js/footer.js
@@ -8,10 +8,10 @@ class Footer extends HTMLElement {
   connectedCallback() {
     const template = document.createElement('template');
     template.innerHTML = `
-        <style>
-          * {
-    box-sizing: border-box;
-    margin: 0;
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
 }
 
 .footer {
@@ -94,82 +94,84 @@ class Footer extends HTMLElement {
 .bar__texto{
     padding-right: 15px;
 }
-        </style>
-        <section class="footer">
-        <div class="footer__logo">
-          <img src="/public/images/footer_logo.png" alt="Logo Footer" />
-          <p>Calle 33 sur # 50a 12</p>
-          <h4>Síguenos:</h4>
-            <div class="footer__redes">
-                <a href="#" class="footer__IG"
-                    ><img src="/public/images/footer_instagram.png" alt="Instagram logo"
-                /></a>
-                <a href="#" class="footer__X"
-                    ><img src="/public/images/X_footer.png" alt="X logo"
-                /></a>
-                <a href="#" class="footer__facebook"
-                    ><img src="/public/images/facebook_footer.png" alt="Instagram logo"
-                /></a>
-            </div>
+</style>
+<footer>
+  <section class="footer">
+    <div class="footer__logo">
+      <img src="/public/images/footer_logo.png" alt="Logo Footer" />
+      <p>Calle 33 sur # 50a 12</p>
+      <h4>Síguenos:</h4>
+        <div class="footer__redes">
+            <a href="#" class="footer__IG"
+                ><img src="/public/images/footer_instagram.png" alt="Instagram logo"
+            /></a>
+            <a href="#" class="footer__X"
+                ><img src="/public/images/X_footer.png" alt="X logo"
+            /></a>
+            <a href="#" class="footer__facebook"
+                ><img src="/public/images/facebook_footer.png" alt="Instagram logo"
+            /></a>
         </div>
-        <div class="footer__perros">
-          <ul>
-            <li><b>Productos para perros</b></li>
-            <br />
-            <li><a href="#">Alimento</a></li>
-            <li><a href="#">Accesorios</a></li>
-            <li><a href="#">Cuidado e higiene </a></li>
-            <li><a href="#">Limpieza</a></li>
-            <li><a href="#">Snacks</a></li>
-            <li><a href="#">Juguetes</a></li>
-            <li><a href="#">Comederos y bebederos</a></li>
-          </ul>
-        </div>
-        <div class="footer__gatos">
-          <ul>
-            <li><b>Productos para gatos</b></li>
-            <br />
-            <li><a href="#">Alimento</a></li>
-            <li><a href="#">Accesorios</a></li>
-            <li><a href="#">Cuidado e higiene</a></li>
-            <li><a href="#">Limpieza</a></li>
-            <li><a href="#">Snacks</a></li>
-            <li><a href="#">Juguetes</a></li>
-            <li><a href="#">Comederos y bebederos</a></li>
-            <li><a href="#">Gimnasios</a></li>
-          </ul>
-        </div>
-        <div class="footer__tiendaOnline">
-          <ul>
-            <li><b>Tienda online</b></li>
-            <br />
-            <li><a href="#">Ofertas</a></li>
-            <li><a href="#">Formas de entrega</a></li>
-            <li><a href="#">Envío programado</a></li>
-            <li><a href="#">Recoge en tienda</a></li>
-          </ul>
-        </div>
-        <div class="footer__enlaces">
-          <ul>
-            <li><b>Enlaces de interés</b></li>
-            <br />
-            <li><a href="#">Aviso de privacidad</a></li>
-            <li><a href="#">Politicas de envío</a></li>
-            <li><a href="#">Cambios y devoluciones</a></li>
-            <li><a href="#">Garantía de productos</a></li>
-            <li><a href="#">Preguntas frecuentes</a></li>
-          </ul>
-        </div>
-      </section>
-      <section class="bar">
-        <p>© 2024 Mascotitas pet shop | Todos los derechos reservados.</p>
-        <p class="bar__texto">Paga de forma segura</p>
-        <img src="/public/images/visa_footer.png" alt="Visa logo">
-        <img src="/public/images/mastercard_footer.png" alt="Mastercard logo">
-        <img src="/public/images/americanxpress_footer.png" alt="American Express logo">
-        <img src="/public/images/pse_footer.png" alt="pse logo">
-        <img src="/public/images/nequi_footer.png" alt="nequi logo">
-      </section>
+    </div>
+    <div class="footer__perros">
+      <ul>
+        <li><b>Productos para perros</b></li>
+        <br />
+        <li><a href="#">Alimento</a></li>
+        <li><a href="#">Accesorios</a></li>
+        <li><a href="#">Cuidado e higiene </a></li>
+        <li><a href="#">Limpieza</a></li>
+        <li><a href="#">Snacks</a></li>
+        <li><a href="#">Juguetes</a></li>
+        <li><a href="#">Comederos y bebederos</a></li>
+      </ul>
+    </div>
+    <div class="footer__gatos">
+      <ul>
+        <li><b>Productos para gatos</b></li>
+        <br />
+        <li><a href="#">Alimento</a></li>
+        <li><a href="#">Accesorios</a></li>
+        <li><a href="#">Cuidado e higiene</a></li>
+        <li><a href="#">Limpieza</a></li>
+        <li><a href="#">Snacks</a></li>
+        <li><a href="#">Juguetes</a></li>
+        <li><a href="#">Comederos y bebederos</a></li>
+        <li><a href="#">Gimnasios</a></li>
+      </ul>
+    </div>
+    <div class="footer__tiendaOnline">
+      <ul>
+        <li><b>Tienda online</b></li>
+        <br />
+        <li><a href="#">Ofertas</a></li>
+        <li><a href="#">Formas de entrega</a></li>
+        <li><a href="#">Envío programado</a></li>
+        <li><a href="#">Recoge en tienda</a></li>
+      </ul>
+    </div>
+    <div class="footer__enlaces">
+      <ul>
+        <li><b>Enlaces de interés</b></li>
+        <br />
+        <li><a href="#">Aviso de privacidad</a></li>
+        <li><a href="#">Politicas de envío</a></li>
+        <li><a href="#">Cambios y devoluciones</a></li>
+        <li><a href="#">Garantía de productos</a></li>
+        <li><a href="#">Preguntas frecuentes</a></li>
+      </ul>
+    </div>
+  </section>
+  <section class="bar">
+    <p>© 2024 Mascotitas pet shop | Todos los derechos reservados.</p>
+    <p class="bar__texto">Paga de forma segura</p>
+    <img src="/public/images/visa_footer.png" alt="Visa logo">
+    <img src="/public/images/mastercard_footer.png" alt="Mastercard logo">
+    <img src="/public/images/americanxpress_footer.png" alt="American Express logo">
+    <img src="/public/images/pse_footer.png" alt="pse logo">
+    <img src="/public/images/nequi_footer.png" alt="nequi logo">
+  </section>
+</footer>
       `;
 
     // Clonamos el template y lo añadimos al shadow DOM
